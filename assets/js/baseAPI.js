c7a7b77d017652153601fc2d0c29ee75ac1d7b9d
Jason <378876897@qq.com>
2021-04-11 23:36:09 +0800

    主页代码

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,17 @@
 $.ajaxPrefilter(function (options){
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    //统一未有权限的设置headers
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        options.complete = function(res){ //不论成功失败都会调用complete 回调函数
+            console.log(res.responseJSON)
+            if(res.responseJSON.status == 1 && res.responseJSON.message === "身份认证失败！"){
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+            }
+    }
+   
    }) //ajax 配置对象
